8d79f0ec618642a3ad0c4e9a2e93cf5dc4c522eb
Veronika Maliar <igoodlutsk0710@gmail.com>
2025-12-23 19:15:22 +0200

    commit

--- backend/src/app.js
+++ backend/src/app.js
@@ -48,7 +48,8 @@ const allowedOrigins = [
   "http://localhost:3000",
   "http://127.0.0.1:5500",
   "http://localhost:5500",
-  "http://localhost:5173"
+  "http://localhost:5173",
+  'https://time-app-liart.vercel.app',
 ];
 
 app.use(
